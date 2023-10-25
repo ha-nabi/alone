cd189d38aef02197a80542288c7f82bfcd0859c0
ciu <112779139+kangciu@users.noreply.github.com>
2023-10-26 00:58:14 +0900

    map filter reduce 예제

--- alone/closure.swift
+++ alone/closure.swift
@@ -61,10 +61,9 @@ import Foundation
 //
 //주어진 배열에서 홀수만 골라내서 제곱한 후 모두 더하는 함수를 작성하세요.
 
-// 예시코드
 //func sumOfSquaresOfOdds(array: [Int]) -> Int {
 //    // 여기에 코드를 작성
-//   return array.filter { $0 % 2 == 1}.reduce(0) { $0 + $1 }
+//    return array.filter { $0 % 2 == 1}.map { $0 * $0 }.reduce(0) { $0 + $1 }
 //}
 //
 //let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -78,13 +77,14 @@ import Foundation
 // 예시코드
 //func reverseWithoutVowels(string: String) -> String {
 //    // 여기에 코드를 작성
-//    let char: Set<Character> = ["A","E","I","O","U"]
+//    let char: Set<Character> = ["A","E","I","O","U"] // 모음 만들어주깅
 //    let result = string.uppercased().filter { !char.contains($0) }
+//                     // uppercased 메서드로 대문자 변환 후 모음이 포함되지 않으면 필터링
 //
-//    return String(result.reversed())
+//    return String(result.reversed()) // 스트링타입으로 묶은 후 리버스
 //}
 //
-//let string = "Hello World"
+//let string = "Hello World" // 결과: DLRW LLH
 //print(reverseWithoutVowels(string: "Hello World"))
 
 
@@ -128,15 +128,15 @@ import Foundation
 //
 //주어진 배열에서 각 요소의 개수를 세어서 딕셔너리로 반환하는 함수를 작성하세요.
 
-func countElements(array: [String]) -> [String: Int] {
-     var dict: [String: Int] = [:]
-    Set(array).forEach { alpha in
-        dict[alpha] = (dict[alpha] ?? 0) + array.reduce(0) { $0 + ($1 == alpha ? 1 : 0) }
-    }
-    return dict
-}
-let array3 = ["a", "b", "a", "c", "b", "d"]
-print(countElements(array: array3))
+//func countElements(array: [String]) -> [String: Int] {
+//     var dict: [String: Int] = [:]
+//    Set(array).forEach { alpha in
+//        dict[alpha] = (dict[alpha] ?? 0) + array.reduce(0) { $0 + ($1 == alpha ? 1 : 0) }
+//    }
+//    return dict
+//}
+//let array3 = ["a", "b", "a", "c", "b", "d"]
+//print(countElements(array: array3))
 
 //맵, 필터, 리듀스 예제 06
 //
@@ -148,13 +148,13 @@ print(countElements(array: array3))
 //
 //주어진 배열에서 각 요소의 앞뒤로 "*"을 붙여서 새로운 배열을 반환하는 함수를 작성하세요.
 
-let plusStar: (String) -> String = { "*" + $0 + "*" }
-
-func addStars(array: [String]) -> [String] {
-    return array.map(plusStar)
-}
-let array5 = ["a", "b", "c"]
-print(addStars(array: array5))
+//let plusStar: (String) -> String = { "*" + $0 + "*" }
+//
+//func addStars(array: [String]) -> [String] {
+//    return array.map(plusStar)
+//}
+//let array5 = ["a", "b", "c"]
+//print(addStars(array: array5))
 
 //맵, 필터, 리듀스 예제 08
 //
@@ -177,13 +177,85 @@ print(addStars(array: array5))
 //주어진 배열에서 각 요소를 두 번 반복해서 새로운 배열을 반환하는 함수를 작성하세요.
 
 // 예시코드
-func repeatTwice(array: [String]) -> [String] {
-    // 여기에 코드를 작성
-    (array + array).sorted()
-}
-let array = ["a", "b", "c"]
-print( repeatTwice(array: array))   // ["a", "a", "b", "b", "c", "c"]
+//func repeatTwice(array: [String]) -> [String] {
+//    // 여기에 코드를 작성
+//    (array + array).sorted()
+//}
+//let array = ["a", "b", "c"]
+//print( repeatTwice(array: array))   // ["a", "a", "b", "b", "c", "c"]
 
 //맵, 필터, 리듀스 예제 10
 //
 //주어진 배열에서 각 요소의 길이를 새로운 배열로 반환하는 함수를 작성하세요.
+
+
+//맵, 필터, 리듀스 예제 11
+//
+//주어진 문자열에서 모음(a, e, i, o, u)의 개수를 세는 함수를 작성하세요.
+
+//func countVowels(_ a: String) -> Int {
+//    let char: Set<Character> = ["a", "e", "i", "o", "u"]
+//    
+//    let result = a.lowercased().filter { char.contains($0) }
+//                    // 소문자 변신 후 char 컬렉션에 포함이 되어있다면
+//    return result.count // 카운트 해
+//}
+
+// 예시코드
+//let word = "swift"
+//let vowelCount = countVowels(word)
+//print(vowelCount) // 1
+
+
+//맵, 필터, 리듀스 예제 12
+//
+//주어진 문자열에서 각 알파벳이 몇 번 나오는지 세는 함수를 작성하세요.
+
+//func countAlphabets(_ a: String) -> [Character: Int] {
+//    
+//    let alphabet = a.filter { $0.isLetter } // 문자열만 필터링
+//    
+//    return alphabet.reduce(into: [:]) { (key, value) in
+//        key[value, default: 0] += 1 } // 문자열이 있으면 + 1
+//}
+//
+//// 예시코드
+//let word = "Hello2World!!"
+//let alphabetCount = countAlphabets(word)
+//print(alphabetCount)  // ["e": 1, "o": 2, "r": 1, "H": 1, "W": 1, "d": 1, "l": 3]
+
+
+
+
+
+//맵, 필터, 리듀스 예제 15
+//
+//주어진 문자열에서 모든 소문자를 대문자로 바꾸는 함수를 작성하세요.
+//(보너스: 대문자는 소문자로 소문자는 대문자로 시도해 보세요.)
+
+//func uppercase(_ a: String) -> String { // 대문자
+//    a.uppercased()
+//}
+//
+//func lowercase(_ a: String) -> String { // 소문자
+//    a.lowercased()
+//}
+//
+//// 예시코드
+//let word = "swift"
+//let uppercasedWord = uppercase(word)
+//print(uppercasedWord)       // "SWIFT"
+
+
+//맵, 필터, 리듀스 예제 16
+//
+//주어진 문자열에서 모든 공백을 제거하는 함수를 작성하세요.
+
+//func removeSpaces(_ a: String) -> String {
+//    a.filter { !$0.isWhitespace }
+//}
+////
+//// 예시코드
+//let sentence = "This is a test"
+//let noSpaceSentence = removeSpaces(sentence)
+//print(noSpaceSentence)      // "Thisisatest"
--- alone/main.swift
+++ alone/main.swift
@@ -1337,4 +1337,45 @@ import Foundation
 //amount가 0보다 작으면 NegativeAmount 에러를 발생시킨다.
 //amount가 잔액보다 크면 InsufficientBalance 에러를 발생시킨다.
 
+//extension Array {
+//    func mapp<T>(_ transform: (Int) -> T) -> [T] {
+//        var result: [T] = []
+//        for num in self {
+//            result.append(transform(num as! Int))
+//        }
+//        return result
+//    }
+//    
+//    func reducee(_ initialResult: Int, _ nextPartialResult: (Int, Int) -> Int) -> Int {
+//        var initialResult: Int = initialResult
+//        self.forEach { element in
+//            initialResult = nextPartialResult(initialResult, element as! Int)
+//        }
+//        return initialResult
+//    }
+//    
+//    func filterr(_ isIncluded: (Int) -> Bool) -> [Int] {
+//        var result: [Int] = []
+//        for num in self {
+//            if isIncluded(num as! Int) { result.append(num as! Int) }
+//        }
+//        return result
+//    }
+//    
+//    func forEachh(_ body: (Int) -> Void) {
+//        for num in self {
+//            body(num as! Int)
+//        }
+//    }
+//}
+//var a = [1,2,3,4,5,6,7,8,9,10]
+//print(a.mapp { String($0) + "번이다" })
+//print(a.reducee(1, *))
+//print(a.filterr { $0 % 2 == 0 })
+
+//맵, 필터, 리듀스 예제 14
+//
+//주어진 배열에서 각 요소를 역순으로 정렬하는 함수를 작성하세요.
+//(컬렉션에서 제공하는 역순함수를 사용하지 않고 구현해 보세요.)
+
 
